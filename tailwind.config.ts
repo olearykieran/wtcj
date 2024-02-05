5ef8545d11307fd9a6b662d54960256b54fb6f17
olearykieran <koleary515@gmail.com>
2024-02-05 15:16:20 -0400

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,9 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
