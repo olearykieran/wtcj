f0ae3717442a84e53930488f158e45aad3c55a22
olearykieran <koleary515@gmail.com>
2024-02-05 13:12:43 -0400

    back to firebase, unoptimized images for now

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true, // Enforces React strict mode
   poweredByHeader: false,
   output: "export",
+  images: {
+    unoptimized: true, // Disables Next.js' built-in Image Optimization
+  },
 };
 
 export default nextConfig;
