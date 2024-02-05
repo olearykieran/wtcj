e39a7f878611592d573cd8f3d13bf025f7919e72
olearykieran <koleary515@gmail.com>
2024-02-05 13:55:39 -0400

    compnents

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+// pages/_app.tsx
+import "../styles/globals.css"; // Adjust the path to your global styles
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* Global components like headers or footers can go here */}
+      <Component {...pageProps} />
+      {/* If you want Footer here, it will appear on every page */}
+    </>
+  );
+}
+
+export default MyApp;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 // src/app/page.tsx
 
-import Header from "../Components/Header";
-import VideoSection from "../Components/VideoSection";
-import MeetTheOwnerSection from "../Components/MeetTheOwnerSection";
-import HowWeCanHelpSection from "../Components/HowWeCanHelpSection";
-import FeaturedSection from "../Components/FeaturedSection";
-import PatientStories from "../Components/PatientStories";
-import Resources from "../Components/Resources";
-import Events from "../Components/Events";
-import Contact from "../Components/Contact";
-import Footer from "../Components/Footer";
-import PopupBar from "../Components/PopupBar";
+import Header from "../components/Header";
+import VideoSection from "../components/VideoSection";
+import MeetTheOwnerSection from "../components/MeetTheOwnerSection";
+import HowWeCanHelpSection from "../components/HowWeCanHelpSection";
+import FeaturedSection from "../components/FeaturedSection";
+import PatientStories from "../components/PatientStories";
+import Resources from "../components/Resources";
+import Events from "../components/Events";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import PopupBar from "../components/PopupBar";
 
 function MyApp({}) {
   return (
