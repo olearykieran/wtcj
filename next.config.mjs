7e36383473c5089a1940f4711dabae57b69389ff
olearykieran <koleary515@gmail.com>
2024-02-05 13:15:56 -0400

    optimizing again

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true, // Enforces React strict mode
   poweredByHeader: false,
   output: "export",
-  images: {
-    unoptimized: true, // Disables Next.js' built-in Image Optimization
-  },
 };
 
 export default nextConfig;
